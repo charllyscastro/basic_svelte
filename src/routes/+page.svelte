34e7c7d9f22892f46f74babfdd8db175cffa71b7
CharllysCastro <charllyscastro@gmail.com>
2023-02-07 15:27:00 -0300

    Iciando componentes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import Button from './Button.svelte'
+</script>
+
 <h1>Home</h1>
+
+<Button label="Componente 1" />
+<Button label="teste" />
+<Button />
 
---
+++ src/routes/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let label = 'Botao 1'
+</script>
+
+<button class="btn">{label}</button>
+
+<style>
+    .btn {
+        color: red;
+    }
+</style>
